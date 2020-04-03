3f3806d1b455645497252df1ec75c675a073e7a2
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-04-03 11:04:42 +0600

    Update Bipartite_Checking.cpp

--- Algorithm and Data Structure/Graph/Bipartite_Checking.cpp
+++ Algorithm and Data Structure/Graph/Bipartite_Checking.cpp
@@ -1,1 +1,17 @@
 
+const int mxN=1e3+5; //Keep an eye on the node numbers
+vector<int>adj[mxN];
+vector<int>color(mxN,-1);
+bool is_bipartite(int u,int c) {
+    color[u]=c;
+    for(int v:adj[u]) {
+        if(color[v]==-1) {
+            if(!is_bipartite(v,c^1)) {
+                return false;  
+            }
+        }
+        else if(color[u]==color[v])
+              return false;
+    }
+    return true;
+}
