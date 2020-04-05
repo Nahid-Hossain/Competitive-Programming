10bbe55263e0dda23fa61a6494476f0d6031e958
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-04-05 17:41:01 +0600

    Update Bipartite_Checking.cpp

--- Algorithm and Data Structure/Graph/Bipartite_Checking.cpp
+++ Algorithm and Data Structure/Graph/Bipartite_Checking.cpp
@@ -1,3 +1,14 @@
+/*
+    Description: If we can color each node using just two colors such that no adjacent node has same color then the graph is called
+                 bipartite graph. Checking bipartiteness of a graph is not hard. A simple dfs traversal in enough to check if a graph
+                 is bipartite or not. First, if the node is not colored yet, then we will color it using the opposite color of its
+                 parent. But if the node has already been colored, then we have to check the color of its parent. If the parent has the 
+                 same color then this  graph is not bipartite, hence we will return false and terminate. Otherwise, after visiting all 
+                 the nodes, if we can color them using two colors, then the graph is bipartite.
+                  
+    Test:https://codeforces.com/contest/1296/problem/E1
+    Complexity: O(v+e)
+*/
 
 const int mxN=1e3+5; //Keep an eye on the node numbers
 vector<int>adj[mxN];
