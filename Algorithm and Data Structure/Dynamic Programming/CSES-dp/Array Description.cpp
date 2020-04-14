5ba230efb9cf5aa76841ca4075dd576bd123cf29
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-04-14 21:11:30 +0600

    Update Array Description.cpp

--- Algorithm and Data Structure/Dynamic Programming/CSES-dp/Array Description.cpp
+++ Algorithm and Data Structure/Dynamic Programming/CSES-dp/Array Description.cpp
@@ -1,3 +1,16 @@
+/*
+    Problem Link:https://cses.fi/problemset/task/1746
+    Idea: Let's define dp[i][j] as the number of ways we can fill the array till ith indices where value is j. For i=0, we can do it
+          separately.If v[i]=0 then we can fill the first position with any value from 1 to m. so dp[0][1...m]=1 if v[0]=0. Otherwise,
+          we have only one option to fill the position 0. so dp[i][x]=1 where x=v[0] and all the other position will contain 0. Now for
+          other indices, we also have two cases. First, if the value of that indices is not zero, then dp[i][x]=dp[i-1][x-1]+dp[i-1][x]
+          +dp[i-1][x+1], where x is the value of that index. Now if the value of that index is zero then, then we will calculate 
+          that for all possible x (from 1 to m). We will think that way: "if the value would x, then dp[i][x]==dp[i-1][x-1]+dp[i-1][x]
+          +dp[i-1][x+1], where x can be 1 to m. The final answer will be Σdp[n-1][m] where m is [1,m].
+    Complexity: O(n*m)
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
