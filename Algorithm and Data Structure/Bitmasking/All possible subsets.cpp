198a3dc41c19d768067393366551d9bf401b45c2
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-04-16 23:40:25 +0600

    Update All possible subsets.cpp

--- Algorithm and Data Structure/Bitmasking/All possible subsets.cpp
+++ Algorithm and Data Structure/Bitmasking/All possible subsets.cpp
@@ -1,1 +1,10 @@
 
+void all_subset(vector<int>&v,int n) {
+    for(int mask=1;mask<(1<<n);mask++) {
+        for(int i=0;i<n;i++) {
+            if(mask>>i&1) {
+                //this element is included in this subset
+            }
+        }
+    }
+}
