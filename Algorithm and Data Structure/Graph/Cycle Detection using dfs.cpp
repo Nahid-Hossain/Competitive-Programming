6202d52721b1a791b8016dfa45e71de8ca66b6b6
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-05-05 11:43:15 +0600

    Update Cycle Detection using dfs.cpp

--- Algorithm and Data Structure/Graph/Cycle Detection using dfs.cpp
+++ Algorithm and Data Structure/Graph/Cycle Detection using dfs.cpp
@@ -1,3 +1,11 @@
+/*
+    Description: By using dfs, we can easily find if a graph has cycle or not. First we will mark all nodes by 0. If a node has marked
+    by 0, then this node is not visited yet. We will mark all the nodes as 1 that are under processed in the call stack. If a node has 
+    finished processing, then we will mark it as 2. During dfs traversal, if we get back to a node that are still in the call stack or 
+    other words if visited[u]=1, then this graph must be cyclic.
+                 
+    Complexity: O(v+e).
+*/
 const int mxN=2e5+10;
 vector<int>adj[mxN];
 vector<int>visited(mxN,0);
