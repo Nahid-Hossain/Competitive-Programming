37677bbdd76bd3802b10bd51fb097edfb42f4fb3
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-05-15 21:49:02 +0600

    Update SRM-728-Div2-500.cpp

--- Algorithm and Data Structure/Dynamic Programming/Topcoder-Dp/SRM-728-Div2-500.cpp
+++ Algorithm and Data Structure/Dynamic Programming/Topcoder-Dp/SRM-728-Div2-500.cpp
@@ -1,3 +1,13 @@
+/*
+    Problem Link: https://community.topcoder.com/stat?c=problem_statement&pm=14809&rd=17064&rm=330921&cr=40543852
+    Idea: Let dp[i][j] means the total numbers of increasing subsequence till index i starting with j and a[i]<=j<=b[i] must be hold.
+          Let a[i]=L and b[i]=R. Now we can calculate for index 0 separately.dp[0][j]=1 for all j from L to R inclusive. Now for other 
+          indices, if we start our increasing subsequence with the value L, then dp[i][L]=sum of(dp[i-1][1...L-1] since the subsequence
+          must be strictly increasing. Now the subsequence can start from any value from L to R. If we know dp[i][L] then,
+          dp[i][L+1]=dp[i][L]+dp[i-1][L] because dp[i][L+1]=sum of(dp[i-1][1...L] and dp[i][L]=sum of(dp[i-1][1...L-1]).So 
+          dp[i]L+1]=dp[i][L]+dp[i-1][L]. The final answer will be sum of(dp[n-1][0...max(R)].
+    Complexity: O(n*max(R).
+*/
 #include<bits/stdc++.h>
 using namespace std;
 const int mod=998244353;
