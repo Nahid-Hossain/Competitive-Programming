d01ed298ba3b9ac6dadccd26715422d19b49caac
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-06-10 23:20:39 +0600

    Create Disjoint Set Union.cpp

---
+++ Algorithm and Data Structure/Graph/Disjoint Set Union.cpp
@@ -0,0 +1,22 @@
+struct dsu {
+    int n;
+    vector<int> p,sz;
+    dsu(int _n) : n(_n) {
+        p.resize(n);
+        sz.resize(n);
+        iota(p.begin(),p.end(), 0);
+    }
+    inline int find(int u) {
+        return (u== p[u] ? u : (p[u]=find(p[u])));
+    }
+    inline bool merge(int u, int v) {
+        u=find(u),v=find(v);
+        if(sz[u]<sz[v]) swap(u,v);
+        if(u!=v) {
+           p[v]=u;
+           sz[u]+=sz[v];
+           return true;
+        }
+        return false;
+    }
+};
