a3adcbac034855b4bd2769658f69331ff64df3f2
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-08-02 16:28:57 +0600

    Update segmented sieve.cpp

--- Algorithm and Data Structure/Number Theory/segmented sieve.cpp
+++ Algorithm and Data Structure/Number Theory/segmented sieve.cpp
@@ -12,14 +12,16 @@ const int limit=32000;
 bitset<limit>is_prime;
 vector<int>prime;
 void sieve() {
-    //keep an eye on the limit
     is_prime.set();
     is_prime[0]=is_prime[1]=0;
     for(int i=4;i<limit;i+=2)
         is_prime[i]=0;
-    for(int i=3;i*i<limit;i+=2)
-        for(int j=i*i;j<limit;j+=i*2)
+    for(int i=3;i*i<limit;i+=2) {
+        if(is_prime[i]) {
+            for(int j=i*i;j<limit;j+=i*2)
             is_prime[j]=0;
+        }
+    }   
     for(int i=2;i<limit;i++)
         if(is_prime[i])
             prime.push_back(i);
