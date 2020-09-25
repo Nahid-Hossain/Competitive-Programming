0d41034ed89dddf24f44f811af96583eb535c88d
Nahid-Hossain <34573914+Nahid-Hossain@users.noreply.github.com>
2020-09-25 18:58:09 +0600

    Create BYTESM2-iterative.cpp

---
+++ Algorithm and Data Structure/Dynamic Programming/spoj/BYTESM2-iterative.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    int t; cin>>t;
+    while (t--) {
+        int h,w; cin>>h>>w;
+        int ans=0;
+        vector<vector<int>>v(h,vector<int>(w));
+        vector<vector<int>>dp(h,vector<int>(w));
+        for(int i=0;i<h;i++) {
+            for(int j=0;j<w;j++) cin>>v[i][j];
+        }
+        for(int i=0;i<h;i++) {
+            for(int j=0;j<w;j++) {
+                dp[i][j]=max({(i-1<0?0:dp[i-1][j]),(i-1<0 or j-1<0?0:dp[i-1][j-1]),(i-1<0 or j+1>=w?0:dp[i-1][j+1])})+v[i][j];
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
